37e328a799d9a9997fd65e7517da2a00941dbd8a
Larsonkv <77larsonkv@gmail.com>
2020-06-24 19:37:05 -0300

    Método desenhar

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 
 public class JogoDaVelha {
-	
-	private JogoDaVelha_Mapa jogoMapa;
+
+	private static JogoDaVelha_Mapa jogoMapa;
 	private JogoDaVelha_PC jogoPc;
 	private JogoDaVelha_Jogador jogoJogador;
+
+	protected void jogar(Scanner teclado) {
+		int contaJogadas = 0;
+		
+		jogoMapa.limpaMapa();
+		
 	
-	protected void jogar(Scanner teclado) {}
-	
-	public static void main(String[] args) {}
-	
+		
+		while(contaJogadas <= 9) {
+			
+		}
+
+		contaJogadas++;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+
+		
+		teclado.close();
+	}
 }
+
+	
--- src/JogoDaVelha_Mapa.java
+++ src/JogoDaVelha_Mapa.java
@@ -3,11 +3,23 @@ public class JogoDaVelha_Mapa {
 	
 	private char[][] mapa;
 
-	public int sortear(int inicio, int fim) { return 0;}
+	// Método sortear dado pelo Dalton
+	private static int sortear(int inicio, int fim) {
+		return (int) ((Math.random()*fim)-inicio);
+	}
 	
 	public void limpaMapa() {}
 	
-	public void desenha(int jogada) {}
+	public void desenha(int jogada) {
+		
+		System.out.println("----------  Jogada: "+jogada);
+		System.out.println("|  |  |  |");
+		System.out.println("----------");
+		System.out.println("|  |  |  |");
+		System.out.println("----------");
+		System.out.println("|  |  |  |");
+		System.out.println("--------------------");
+	}
 	
 	public boolean jogar(int l, int c, char jogador) { return false;}
 	
